5e96ad69d8443aa5ab6f855b7138c50c9868b063
Atulkumar96 <atul.kumar9631@gmail.com>
2024-09-28 17:12:30 +0530

    OAuth2 - UserDetails from Db during login validation, Spring OAuth2 validates and allows login

--- src/main/java/com/ecom/authenticationservice/securities/CustomOurDbUserDetailsService.java
+++ src/main/java/com/ecom/authenticationservice/securities/CustomOurDbUserDetailsService.java
@@ -1,12 +1,31 @@
 package com.ecom.authenticationservice.securities;
 
+import com.ecom.authenticationservice.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import com.ecom.authenticationservice.models.User;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class CustomOurDbUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public CustomOurDbUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Optional<User> userOptional = userRepository.findByEmail(username);
+        if (userOptional.isEmpty()) {
+            throw new UsernameNotFoundException(username+" not found");
+        }
+        User user = userOptional.get();
+
+
+        return new CustomOurDbUserDetails(user);
     }
 }
--- src/main/java/com/ecom/authenticationservice/securities/SecurityConfig.java
+++ src/main/java/com/ecom/authenticationservice/securities/SecurityConfig.java
@@ -88,16 +88,16 @@ public class SecurityConfig { // customize the default configuration
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails userDetails = User.builder()
-                .username("user")
-                .password(bCryptPasswordEncoder.encode("password"))
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(userDetails);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails userDetails = User.builder()
+//                .username("user")
+//                .password(bCryptPasswordEncoder.encode("password"))
+//                .roles("USER")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(userDetails);
+//    }
 
     @Bean
     public RegisteredClientRepository registeredClientRepository() {
